1c9a4e2d5257899f03605fb3369caa7e8671fdbf
Sebastian Pietras <me@spietras.dev>
2025-03-01 21:09:48 +0100

    Updated dependencies

--- src/lib/urls/create-orchid-auth-logout-url/utils.ts
+++ src/lib/urls/create-orchid-auth-logout-url/utils.ts
@@ -1,10 +1,7 @@
 export function getOrchidURL() {
   const scheme = process.env.CROCUS__ORCHID__PUBLIC__SCHEME || "http";
   const host = process.env.CROCUS__ORCHID__PUBLIC__HOST || "localhost";
-  const port =
-    process.env.CROCUS__ORCHID__PUBLIC__PORT === undefined
-      ? 20120
-      : process.env.CROCUS__ORCHID__PUBLIC__PORT;
+  const port = process.env.CROCUS__ORCHID__PUBLIC__PORT ?? 20120;
   const path = (process.env.CROCUS__ORCHID__PUBLIC__PATH || "")
     // Ensure path starts with a slash
     .replace(/^(?!\/)(.*)$/, "/$1")
