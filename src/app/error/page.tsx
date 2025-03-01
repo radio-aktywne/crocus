cbffa2efa205415798feaaf6300512c92bd59e83
Sebastian Pietras <me@spietras.dev>
2025-03-01 21:10:09 +0100

    Updated dependencies (#8)

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,5 +1,5 @@
 import { i18n } from "@lingui/core";
-import { msg, t } from "@lingui/macro";
+import { msg } from "@lingui/core/macro";
 import { Metadata } from "next";
 
 import { ErrorPageMetadata } from "../../components/metadata/error/error-page-metadata";
@@ -15,8 +15,8 @@ export async function generateMetadata(): Promise<Metadata> {
   await loadLocale({ i18n, language });
 
   return {
-    description: t(i18n)(msg({ message: "crocus" })),
-    title: t(i18n)(msg({ message: "Error • crocus" })),
+    description: i18n._(msg({ message: "crocus" })),
+    title: i18n._(msg({ message: "Error • crocus" })),
   };
 }
 
--- src/services/scorpion/main.ts
+++ src/services/scorpion/main.ts
@@ -5,10 +5,7 @@ import { paths } from "./types";
 
 const scheme = process.env.CROCUS__SCORPION__ADMIN__SCHEME || "http";
 const host = process.env.CROCUS__SCORPION__ADMIN__HOST || "localhost";
-const port =
-  process.env.CROCUS__SCORPION__ADMIN__PORT === undefined
-    ? 20001
-    : process.env.CROCUS__SCORPION__ADMIN__PORT;
+const port = process.env.CROCUS__SCORPION__ADMIN__PORT ?? 20001;
 const path = (process.env.CROCUS__SCORPION__ADMIN__PATH || "")
   // Ensure path starts with a slash
   .replace(/^(?!\/)(.*)$/, "/$1")
